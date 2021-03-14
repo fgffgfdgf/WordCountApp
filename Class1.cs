55dc64b9e9a7f9084264d8fbc6cb40ccd7f1893f
Рома <79446769+fgffgfdgf@users.noreply.github.com>
2021-03-14 17:22:50 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿    using System;
+    using System.Collections.Concurrent;
+    using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace ClassLibrary1
+    {
+        public class Dict
+        {
+        static ConcurrentDictionary<string, int> CountWords(string path)
+        {
+            var result = new ConcurrentDictionary<string, int>();
+            Parallel.ForEach(File.ReadLines(path, Encoding.UTF8), line =>
+            {
+                var words = line.Split(new[] { ' ', '-', ':', '.', '"', '!', '?', ',', ';', ')', '(', '\\', '/', '<', '>', '*', '[', ']'}, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var word in words)
+                {
+                    result.AddOrUpdate(word, 1, (_, x) => x + 1);
+                }
+            });
+            return result;
+        }
+        }
+    }
